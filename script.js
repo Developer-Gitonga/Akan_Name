dbea62a55ccc24bf00b916e529685ad34cd822b5
Dev Git <95219841+Developer-Gitonga@users.noreply.github.com>
2022-05-16 20:44:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -57,6 +57,7 @@ function getAkanName () {
   let dayOfWeekNumber = Math.floor((((Number(yearOfBirth.slice(0,2))/4)-2*Number(yearOfBirth.slice(0,2))-1)+
           ((5*Number(yearOfBirth.slice(2,4))/4))+((26*(monthOfBirth+1)/10))+dayOfBirth)%7);
 
+  
   //arrays of Akan names for males & females and days of the week
   let daysOfWeek = [
     "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
